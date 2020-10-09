b601cd2148859aeec3902166261963a5835f5249
jaden <jonghaha7@gmail.com>
2020-10-09 18:35:31 +0900

    차단 관련 속성 추가

--- src/main/java/com/jaden/springboot/bootprj/domain/Person.java
+++ src/main/java/com/jaden/springboot/bootprj/domain/Person.java
@@ -1,8 +1,6 @@
 package com.jaden.springboot.bootprj.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,21 +8,38 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
-@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class Person {
     @Id
     @GeneratedValue
     private Long id;
+    @NonNull
     private String name;
+    @NonNull
     private int age;
     private String hobby;
     private String bloodType;
     private String address;
     private LocalDate birthDay;
     private String job;
-
     @ToString.Exclude
     private String phoneNumber;
+    private boolean block;
+
+
+    /*public boolean equals(Object object){
+        if(object == null)
+            return false;
+
+        Person person = (Person) object;
+
+        if(!person.getName().equals(this.getName()))
+            return false;
+        if(person.getAge() != this.getAge())
+            return false;
+        else return true;
+    }*/
 }
