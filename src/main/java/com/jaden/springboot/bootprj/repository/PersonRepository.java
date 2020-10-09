10288b4966303252f4c81aa4a49dbd0072831caf
jaden <jonghaha7@gmail.com>
2020-10-09 18:42:09 +0900

    Repositroy 어노테이션 관련 설명 comment 추가

--- src/main/java/com/jaden/springboot/bootprj/repository/PersonRepository.java
+++ src/main/java/com/jaden/springboot/bootprj/repository/PersonRepository.java
@@ -2,7 +2,9 @@ package com.jaden.springboot.bootprj.repository;
 
 import com.jaden.springboot.bootprj.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository //jpaRepository 를 extends 하면 자동으로 repository bean으로 등록됨
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
 }
